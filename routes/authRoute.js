b59c92724509259f036bc8e73bf6e6aabbcc9eff
Yoga Adi Saputra <syogaadi75@gmail.com>
2023-01-17 16:57:58 +0700

    deployment

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,7 @@
 const express = require('express');
-// const Users = require('../models/Users.js');
-// const Rooms = require('../models/Rooms.js');
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
+const Users = require('../models/Users.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 // const {
 //   check,
 //   validationResult
